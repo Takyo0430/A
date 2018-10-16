bd5025ee955139753a7ba74c036b163b83550830
Yuichi Takyo <mikamienkon@gmail.com>
2018-10-16 10:04:42 +0900

    A:コメントの追加

--- system1.php
+++ system1.php
@@ -1,12 +1,12 @@
 <?php
 /*************************************************
  * 管理者実行スクリプト
- * 
+ *
  */
 
 define('_ROOT_DIR', __DIR__ . '/');
 require_once _ROOT_DIR . '../php_libs/init.php';
 $controller = new SystemController();
 $controller->run();
-
+//コミットしたんや
 exit;
