658fb7a601f8b41dc3176c8b6fc899615f7e8642
Yuichi Takyo <mikamienkon@gmail.com>
2018-10-23 09:25:51 +0900

    delete coment

--- system1.php
+++ system1.php
@@ -9,10 +9,5 @@ require_once _ROOT_DIR . '../php_libs/init.php';
 $controller = new SystemController();
 $controller->run();
 
-<<<<<<< HEAD
-//ココに追加
-//さよなら
-=======
->>>>>>> 1d2c43ee93bed1bcc51a148009a1f99a8a773095
 
 exit;
