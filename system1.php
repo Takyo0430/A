a9a4f1f00bd687465a68ae6a3add2f9b73758b5d
Yuichi Takyo <mikamienkon@gmail.com>
2018-10-25 11:02:06 +0900

    パスの変更

--- system1.php
+++ system1.php
@@ -5,7 +5,7 @@
  */
 
 define('_ROOT_DIR', __DIR__ . '/');
-require_once _ROOT_DIR . '../php_libs/init.php';
+require_once _ROOT_DIR . 'php_libs/init.php';
 $controller = new SystemController();
 $controller->run();
 
